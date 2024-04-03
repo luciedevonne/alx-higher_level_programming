5f87099c2dda86f5b76efa0be15e573fc234ec5b
luciedevonne <lucielucylucy@gmail.com>
2024-04-03 16:43:31 -0700

    oop

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """creating initialization for square"""
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """creating instances of class"""
+    def __init__(self, size):
+        self.__size = size
+
+
+square =  Square(5)
+print("Size: ",square._Square__size) 
