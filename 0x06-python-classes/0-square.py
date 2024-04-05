53c913d8433d704049a2112860782e27f73a18e0
luciedevonne <lucielucylucy@gmail.com>
2024-04-04 18:08:00 -0700

    correct output

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -7,11 +7,8 @@ class Square:
     '''empty class'''
     pass
 
-# Instantiating Square class
+
 mysquare = Square()
 
-# Printing type of mysquare
 print(type(mysquare))
-
-# Printing dictionary containing instance variables of mysquare
 print(mysquare.__dict__)
