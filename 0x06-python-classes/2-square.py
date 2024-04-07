d752a28f943bc75da1df73fadf3ffc89a95b9ab3
luciedevonne <lucielucylucy@gmail.com>
2024-04-06 22:49:04 -0700

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''creating class'''
+
+
+class Square
+
+    def __init__(self, size=0):
+        '''initialize instances'''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
