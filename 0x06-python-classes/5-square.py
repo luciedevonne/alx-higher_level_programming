2ff8b338c383851f0d00ae10cabb5d6956340a5a
luciedevonne <lucielucylucy@gmail.com>
2024-04-09 00:57:33 -0700

    pep8 validation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     '''Instantiation whereby:
-    size must be an integer, 
+    size must be an integer,
     otherwise raise a TypeError exception with the message
     size must be an integer
     if size is less than 0,
