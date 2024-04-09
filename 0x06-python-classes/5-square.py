bdbc7d66a8a3ee0556de3f041b82c07ddf7ea776
luciedevonne <lucielucylucy@gmail.com>
2024-04-09 00:42:59 -0700

    initial commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''defining class Square'''
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, integer):
+            raise TypeError('size must be an integer')
+        elif size<0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+
+        else:
+            for _ in range(self.__size):
+                print('#' * self.__size)
+
