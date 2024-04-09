d44ec7b1422053aca32f9479fefe7e28534a6705
luciedevonne <lucielucylucy@gmail.com>
2024-04-09 00:56:31 -0700

    pep8 validation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,9 +4,11 @@
 
 class Square:
     '''Instantiation whereby:
-    size must be an integer, otherwise raise a TypeError exception with the message
+    size must be an integer, 
+    otherwise raise a TypeError exception with the message
     size must be an integer
-    if size is less than 0, raise a ValueError exception with the message size must be >= 0
+    if size is less than 0,
+    raise a ValueError exception with the message size must be >= 0
     '''
 
     def __init__(self, size=0):
