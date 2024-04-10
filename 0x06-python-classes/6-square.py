01e64d583b7440e307dd4b9b84274da7ba896b0a
luciedevonne <lucielucylucy@gmail.com>
2024-04-10 04:54:22 -0700

    pep8 validation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,7 @@ class Square:
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
-            position (tuple, optional): The position of the square. Defaults to (0, 0).
+            position (tuple): The position of the square. Defaults to (0, 0)
         '''
         self.size = size
         self.position = position
